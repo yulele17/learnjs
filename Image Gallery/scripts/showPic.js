7fc565aa2e37de080e0d6a0b7d42ee463f9ea227
yulele17 <yulele17@126.com>
2016-02-04 16:11:08 +0800

    second commit

--- Image Gallery/scripts/showPic.js
+++ Image Gallery/scripts/showPic.js
@@ -1,5 +1,17 @@
 function showPic(whichpic) {
   var source = whichpic.getAttribute("href");
+  var text = whichpic.getAttribute('title');
+  var description = document.getElementById('description');
   var placeholder = document.getElementById('placeholder');
   placeholder.setAttribute("src",source);
+  description.firstChild.nodeValue = text;
 }
+function countBodyChildren(){
+  var body_element = document.getElementsByTagName('body')[0];
+  var description = document.getElementById('description');
+  alert(body_element.childNodes.length);
+  alert(body_element.childNodes[0].nodeType);
+  alert(description.firstChild.nodeValue);
+}
+
+window.onload = countBodyChildren;
